47e7474eadf3a3cfb7420fb2475146bc05da3f10
Franciszek <Franciszek@com>
2020-10-06 20:45:35 +0200

    -kurs 31+ ?

--- src/main/java/com/example/todoapp/controller/TaskController.java
+++ src/main/java/com/example/todoapp/controller/TaskController.java
@@ -16,7 +16,7 @@ public class TaskController {
         this.repository = repository;
     }
 
-    @GetMapping("/tasks")
+    @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
     ResponseEntity<?> readAllTasks(){
         logger.warn("Exposing all the tasks!");
         return ResponseEntity.ok(repository.findAll());
