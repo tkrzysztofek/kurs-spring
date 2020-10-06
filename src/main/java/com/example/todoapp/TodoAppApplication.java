c758fc15d56f0b89d3624568a44df5050098ed4e
Franciszek <Franciszek@com>
2020-10-06 22:22:47 +0200

    -kurs 38

--- src/main/java/com/example/todoapp/TodoAppApplication.java
+++ src/main/java/com/example/todoapp/TodoAppApplication.java
@@ -3,13 +3,11 @@ package com.example.todoapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
-import org.springframework.validation.Validator;
+import javax.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
-public class TodoAppApplication implements RepositoryRestConfigurer {
+public class TodoAppApplication   {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoAppApplication.class, args);
@@ -18,12 +16,6 @@ public class TodoAppApplication implements RepositoryRestConfigurer {
 	@Bean
 	Validator validator() {
 		return new LocalValidatorFactoryBean();
-	}
-
-	@Override
-	public void configureValidatingRepositoryEventListener(ValidatingRepositoryEventListener validatingListener) {
-		validatingListener.addValidator("beforeCreate", validator());
-		validatingListener.addValidator("beforeSave", validator());
 	}
 
 }
--- src/main/java/com/example/todoapp/controller/TaskController.java
+++ src/main/java/com/example/todoapp/controller/TaskController.java
@@ -1,13 +1,17 @@
 package com.example.todoapp.controller;
 
+import com.example.todoapp.model.Task;
 import com.example.todoapp.model.TaskRepository;
-import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
-@RepositoryRestController
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
 public class TaskController {
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
     private final TaskRepository repository;
@@ -17,9 +21,25 @@ public class TaskController {
     }
 
     @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
-    ResponseEntity<?> readAllTasks(){
+    ResponseEntity<List<Task>> readAllTasks() {
         logger.warn("Exposing all the tasks!");
         return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping(value = "/tasks")
+    ResponseEntity<?> readAllTasks(Pageable page) {
+        logger.info("Custom pagable");
+        return ResponseEntity.ok(repository.findAll(page).getContent());
+    }
+
+    @PutMapping("/tasks/{id}")
+    ResponseEntity<?> updateTask(@PathVariable int id, @RequestBody @Valid Task toUpdate) {
+        if (!repository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        toUpdate.setId(id);
+        repository.save(toUpdate);
+        return ResponseEntity.noContent().build();
     }
 
 
