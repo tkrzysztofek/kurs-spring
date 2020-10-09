ddc4a1971f732b2b63a3fd0117ebb294ddf349c9
krzysztofekt <tomasz.krzysztofek@comarch.com>
2020-10-09 16:50:44 +0200

    - 50

---
+++ src/main/java/com/example/todoapp/controller/InfoController.java
@@ -0,0 +1,29 @@
+package com.example.todoapp.controller;
+
+import com.example.todoapp.TaskConfigurationProperties;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InfoController {
+
+    private DataSourceProperties dataSource;
+    private TaskConfigurationProperties myProp;
+
+    public InfoController(DataSourceProperties dataSource, TaskConfigurationProperties myProp) {
+        this.dataSource = dataSource;
+        this.myProp = myProp;
+    }
+
+    @GetMapping("/info/url")
+    String url(){
+        return dataSource.getUrl();
+    }
+
+    @GetMapping("/info/prop")
+    boolean myProp(){
+        return myProp.getTemplate().isAllowMultipleTasks();
+    }
+
+}
